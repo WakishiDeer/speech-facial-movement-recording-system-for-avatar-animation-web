2add13e36e87534046fc47dc6027ef9893be7e44
WakishiDeer <by9132@gmail.com>
2022-12-07 00:52:50 +0900

    add: check ipv4 address is valid or not

--- plugins/utils.js
+++ plugins/utils.js
@@ -110,3 +110,8 @@ export function isLastSlide(updatedSlideIndex, isNext, isPrev, slideLength) {
     return updatedSlideIndex === slideLength - 2;
   }
 }
+
+export function isValidIPv4(ip) {
+  const regex = new RegExp("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+  return regex.test(ip);
+}
